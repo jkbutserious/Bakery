bfb1425b453956aa3bd0efab493608631ec55755
Jeff Kim <kim2jy@hotmail.com>
2020-07-17 13:41:48 -0700

    Add fourth test to calculate Pastries price with no discount

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,14 +1,26 @@
 using System;
-using Order;
 
 namespace Bakery.Models
 {
   public class Bread
   {
-    public int AddBreadToOrder(int userLoaves)
+    public double PricePerLoaf { get; set; } = 5;
+    public double Discount
     {
-      Order.Loaves = userLoaves;
-      return Order.Loaves;
+      get
+      {
+        return Math.Floor(Count / 3.0) * PricePerLoaf;
+      }
     }
+    public double BreadTotal
+    {
+      get
+      {
+        return PricePerLoaf * Count - Discount;
+      }
+    }
+    public int Count { get; set; }
+
+
   }
 }
---
+++ Bakery/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var Key = Console.ReadKey(true);
+            if (Key.Key == ConsoleKey.B)
+            {
+
+            }
+        }
+    }
+}
